0a8bbce89d98118b1d1bbecc6b5d34e0567c1c3e
Terrell Ibanez <9453223+ibanezt@users.noreply.github.com>
2020-03-08 05:13:00 -0400

    Add lookbook route / initial page

--- Hackathon/flask/nona/__init__.py
+++ Hackathon/flask/nona/__init__.py
@@ -69,12 +69,16 @@ def create_app(test_config=None):
     def categories():
         return render_template('categories.html')
 
+    @app.route('/lookbook')
+    def lookbook():
+        return render_template('lookbook.html')
+
     from . import db
     db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
-    
+
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/blog', endpoint='blog.index')
